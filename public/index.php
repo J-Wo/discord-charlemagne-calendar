5168beb547c8b0faa4d404f634871b4a76dbab5a
J-Wo <J-Wo@users.noreply.github.com>
2026-08-11 20:36:27 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,7 +12,6 @@ $config = json_decode(file_get_contents(dirname(__FILE__)."/../_config.json"),fa
 Flight::set("config",$config);
 
 
-
 Flight::route('/', function() {
     Flight::view()->set('auth', $_SESSION["login"] ?? false);
     Flight::view()->set('loadingEndpoint', Flight::get("config")->loadingEndpoint);
@@ -45,6 +44,7 @@ Flight::route('/loadEvents', function() {
 	
 	Flight::json($events->getIterator()->getArrayCopy());
 });
+
 Flight::route('/syncEvent', function() {
     if(Flight::get("config")->customToken !== Flight::request()->query['token']) die("no");
     
@@ -165,4 +165,60 @@ Flight::route('/deleteEvent', function() {
     print "cancelled";
     Flight::stop();
 });
+
+// iCal (.ics) Feed Route for iOS Calendar Subscription
+Flight::route('/cal.ics', function() {
+    $config = Flight::get("config");
+    $pass = Flight::request()->query['password'] ?? '';
+    
+    // Auth check against webPassword
+    if ($pass !== $config->webPassword) {
+        Flight::halt(401, "Unauthorized");
+        return;
+    }
+
+    header('Content-Type: text/calendar; charset=utf-8');
+    header('Content-Disposition: inline; filename="calendar.ics"');
+
+    $ics  = "BEGIN:VCALENDAR\r\n";
+    $ics .= "VERSION:2.0\r\n";
+    $ics .= "PRODID:-//Discord Charlemagne Calendar//EN\r\n";
+    $ics .= "CALSCALE:GREGORIAN\r\n";
+    $ics .= "X-WR-CALNAME:Clan Events\r\n";
+
+    $database = new \JamesMoss\Flywheel\Config(dirname(__FILE__) . '/../DB');
+    $repo = new \JamesMoss\Flywheel\Repository('events', $database);
+    $events = $repo->query()->execute();
+
+    foreach ($events as $event) {
+        $startTime = strtotime($event->start ?? 'now');
+        $endTime = $startTime + 3600; // default 1 hour duration
+        
+        $dtStart = date('Ymd\THis\Z', $startTime);
+        $dtEnd = date('Ymd\THis\Z', $endTime);
+        $title = $event->title ?? 'Clan Event';
+        $desc = $event->body ?? '';
+        if (!empty($event->attendees) && is_array($event->attendees)) {
+            $desc .= "\n\nGuardians: " . implode(', ', $event->attendees);
+        }
+        $id = $event->id ?? md5($title . $dtStart);
+
+        $ics .= "BEGIN:VEVENT\r\n";
+        $ics .= "UID:" . $id . "@charlemagne\r\n";
+        $ics .= "DTSTAMP:" . date('Ymd\THis\Z') . "\r\n";
+        $ics .= "DTSTART:" . $dtStart . "\r\n";
+        $ics .= "DTEND:" . $dtEnd . "\r\n";
+        $ics .= "SUMMARY:" . addcslashes($title, ",\\;") . "\r\n";
+        if (!empty($desc)) {
+            $ics .= "DESCRIPTION:" . addcslashes($desc, ",\\;") . "\r\n";
+        }
+        $ics .= "END:VEVENT\r\n";
+    }
+
+    $ics .= "END:VCALENDAR";
+
+    echo $ics;
+    Flight::stop();
+});
+
 Flight::start();
